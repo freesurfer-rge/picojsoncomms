c0f6b6bff832e974873d15b7585a253a94dc13ba
Richard Edgar <freesurfer.rge@gmail.com>
2023-12-30 11:26:35 -0500

    Starting on the host side

---
+++ host/simple_comms.py
@@ -0,0 +1,30 @@
+import argparse
+import asyncio
+import logging
+import pathlib
+
+_logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--pico_device",
+        type=pathlib.Path,
+        help="Path (in /dev/) to the Pico",
+        required=True,
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+async def main():
+    args = parse_args()
+    _logger.info(f"pico_device: {args.pico_device}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- pico/lib/jsoncommunicator.py
+++ pico/lib/jsoncommunicator.py
@@ -8,6 +8,7 @@ import machine
 
 import aio_queue
 
+
 class JSONCommunicator:
     def __init__(self):
         self._incoming = aio_queue.Queue()
@@ -21,7 +22,7 @@ class JSONCommunicator:
     @property
     def poll_sleep_secs(self):
         return self._poll_sleep_secs
-    
+
     @poll_sleep_secs.setter
     def poll_sleep_secs(self, value):
         self._poll_sleep_secs = value
@@ -47,56 +48,85 @@ class JSONCommunicator:
             if len(result) > 0:
                 poll_tuple = result[0]
                 if poll_tuple[1] != select.POLLIN:
-                    self.send_log(dict(level="error", message=f"Unexpected poll_tuple[1]: {poll_tuple[1]}"))
+                    self.send_log(
+                        dict(
+                            level="error",
+                            message=f"Unexpected poll_tuple[1]: {poll_tuple[1]}",
+                        )
+                    )
                 else:
                     json_line = poll_tuple[0].readline()
                     try:
                         recv_obj = json.loads(json_line)
                         await self._handle_received(recv_obj)
                     except Exception as e:
-                        self.send_log(dict(level="error", message=f"Failed to decode: {json_line}"))
+                        self.send_log(
+                            dict(
+                                level="error", message=f"Failed to decode: {json_line}"
+                            )
+                        )
             await asyncio.sleep(self.poll_sleep_secs)
 
     async def _handle_received(self, recv_obj):
         # On startup, the 'SYN' message won't know the target_id
         if "target_id" in recv_obj:
             if recv_obj["target_id"] != self._id:
-                self.send_log(dict(level="error", message=f"Missent object: {json.dumps(recv_obj)}"))
+                self.send_log(
+                    dict(
+                        level="error", message=f"Missent object: {json.dumps(recv_obj)}"
+                    )
+                )
                 return
-            
+
         if "type" not in recv_obj or "payload" not in recv_obj:
-            self.send_log(dict(level="error", message=f"Missing type or payload in object: {json.dumps(recv_obj)}"))
+            self.send_log(
+                dict(
+                    level="error",
+                    message=f"Missing type or payload in object: {json.dumps(recv_obj)}",
+                )
+            )
             return
-        
+
         if recv_obj["type"] == "user":
             await self._incoming.put(recv_obj["payload"])
         elif recv_obj["type"] == "sys":
             await self._handle_sys(recv_obj["payload"])
         else:
-            self.send_log(dict(level="error", message=f"Unrecognised message type: {recv_obj}"))
+            self.send_log(
+                dict(level="error", message=f"Unrecognised message type: {recv_obj}")
+            )
 
     async def _handle_sys(self, sys_payload):
         if "kind" not in sys_payload:
-            self.send_log(dict(level="error", message=f"Missing kind on sys_payload: {json.dumps(sys_payload)}"))
+            self.send_log(
+                dict(
+                    level="error",
+                    message=f"Missing kind on sys_payload: {json.dumps(sys_payload)}",
+                )
+            )
             return
-        
+
         response = None
         send_ready = False
         if sys_payload["kind"] == "SYN":
             response = dict(kind="ACK")
             send_ready = True
         else:
-            self.send_log(dict(level="error", message=f"Unknown kind on sys_payload: {json.dumps(sys_payload)}"))
+            self.send_log(
+                dict(
+                    level="error",
+                    message=f"Unknown kind on sys_payload: {json.dumps(sys_payload)}",
+                )
+            )
             return
-        
+
         message = dict(sender_id=self._id, type="sys", payload=response)
         print(json.dumps(message))
         if send_ready:
             self._ready.set()
-            
 
     @classmethod
-    async def create(cls) ->'JSONCommunicator':
+    async def create(cls) -> "JSONCommunicator":
         comm = JSONCommunicator()
         await comm._ready.wait()
         return comm
