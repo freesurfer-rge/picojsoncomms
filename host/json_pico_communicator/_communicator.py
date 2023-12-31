a6d73c1d69980e414edd7b5e13923ebacf2dc15d
Richard Edgar <freesurfer.rge@gmail.com>
2023-12-31 18:34:54 -0500

    blacken

--- host/json_pico_communicator/_communicator.py
+++ host/json_pico_communicator/_communicator.py
@@ -18,7 +18,7 @@ class JSONCommunicator:
         # self._tty = io.TextIOWrapper(
         #    io.FileIO(os.open(pico_path, os.O_NOCTTY | os.O_RDWR), "r+"),
         #    line_buffering=True,
-        #)
+        # )
         self._tty = serial.Serial(str(pico_path))
         self._loop = asyncio.get_running_loop()
         self._incoming = asyncio.Queue()
@@ -26,9 +26,8 @@ class JSONCommunicator:
         self._thread = None
 
     @property
-    def pico_id(self)-> str:
+    def pico_id(self) -> str:
         return self._pico_id
-
 
     def _tty_reader(self):
         for line in iter(self._tty.readline, None):
@@ -42,21 +41,23 @@ class JSONCommunicator:
             else:
                 time.sleep(0.01)
 
-    def _handle_incoming(self, incoming_obj: dict[str,any]):
+    def _handle_incoming(self, incoming_obj: dict[str, any]):
         if "sender_id" not in incoming_obj:
             _logger.error(f"sender_id missing: {incoming_obj}")
             return
-        
+
         if incoming_obj["sender_id"] != self.pico_id:
             _logger.error(f"Bad sender_id: {incoming_obj}")
             return
-        
+
         if "type" not in incoming_obj or "payload" not in incoming_obj:
             _logger.error(f"Missing type or payload: {incoming_obj}")
             return
-        
+
         if incoming_obj["type"] == "user":
-            asyncio.run_coroutine_threadsafe(self._incoming.put(incoming_obj["payload"]), self._loop)
+            asyncio.run_coroutine_threadsafe(
+                self._incoming.put(incoming_obj["payload"]), self._loop
+            )
         elif incoming_obj["type"] == "log":
             _logger.info(f"Log from Pico: {incoming_obj}")
         else:
@@ -67,22 +68,22 @@ class JSONCommunicator:
         return nxt
 
     def send(self, payload: dict[str, any]):
-        message = dict(type='user', target_id=self.pico_id, payload=payload)
+        message = dict(type="user", target_id=self.pico_id, payload=payload)
         msg_str = json.dumps(message) + "\n"
-        self._tty.write(msg_str.encode('utf-8'))
+        self._tty.write(msg_str.encode("utf-8"))
 
     @classmethod
-    async def create(cls, pico_path: pathlib.Path) -> 'JSONCommunicator':
+    async def create(cls, pico_path: pathlib.Path) -> "JSONCommunicator":
         comm = JSONCommunicator(pico_path)
         syn_msg = dict(type="sys", payload=dict(kind="SYN"))
         syn_str = json.dumps(syn_msg) + "\n"
-        comm._tty.write(syn_str.encode('utf-8'))
+        comm._tty.write(syn_str.encode("utf-8"))
         response = comm._tty.readline().strip()
         _logger.info(f"Response: {response}")
         resp_obj = json.loads(response)
         comm._pico_id = resp_obj["sender_id"]
-        assert resp_obj["type"]=="sys"
-        assert resp_obj["payload"]["kind"] =="ACK"
+        assert resp_obj["type"] == "sys"
+        assert resp_obj["payload"]["kind"] == "ACK"
         _logger.info(f"Established communcation with {comm.pico_id}")
 
         comm._thread = threading.Thread(target=comm._tty_reader)
